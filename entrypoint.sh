333f1e0b89882b65d428d7f96b3e25eee2caeac0
Mike Ball <mikedball@gmail.com>
2021-06-11 11:51:45 -0400

    correct env var names
    Signed-off-by: Mike Ball <mikedball@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,14 +12,14 @@ diff_tool_template="git \
     --ignore-all-space \
     {{ .V1 }} {{ .V2 }}"
 
-giturl="${1:-${INPUT_REPO}}"
-v1ref="${2:-${INPUT_V1REF}}"
-v2ref="${3:-${INPUT_V2REF}}"
+git_url="${1:-${INPUT_GIT_URL}}"
+v1_ref="${2:-${INPUT_V1_REF}}"
+v2_ref="${3:-${INPUT_V2_REF}}"
 subpath="${4:-${INPUT_SUBPATH}}"
 
 terrajux \
   -difftool "${diff_tool_template}" \
-  "${giturl}" \
-  "${v1ref}" \
-  "${v2ref}" \
+  "${git_url}" \
+  "${v1_ref}" \
+  "${v2_ref}" \
   "${subpath}"
--- test.sh
+++ test.sh
@@ -2,20 +2,20 @@
 
 set -o nounset -e
 
-repo="https://github.com/terraform-aws-modules/terraform-aws-iam.git"
-v1ref="v3.15.0"
-v2ref="v3.16.0"
+git_url="https://github.com/terraform-aws-modules/terraform-aws-iam.git"
+v1_ref="v3.15.0"
+v2_ref="v3.16.0"
 subpath="."
 
 /entrypoint.sh \
-  "${repo}" \
-  "${v1ref}" \
-  "${v2ref}" \
+  "${git_url}" \
+  "${v1_ref}" \
+  "${v2_ref}" \
   "${subpath}"
 
-INPUT_REPO="${repo}" \
-INPUT_V1REF="${v1ref}" \
-INPUT_V2REF="${v2ref}" \
+INPUT_GIT_URL="${git_url}" \
+INPUT_V1_REF="${v1_ref}" \
+INPUT_V2_REF="${v2_ref}" \
 INPUT_SUBPATH="${subpath}" \
 /entrypoint.sh
 
