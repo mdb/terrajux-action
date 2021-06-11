53ba47d5d662e19d6e8bdf2296944c5d2cd4d4ff
Mike Ball <mikedball@gmail.com>
2021-06-11 10:00:11 -0400

    support local clones
    Signed-off-by: Mike Ball <mikedball@gmail.com>

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 set -o nounset -e
 
-repo="terraform-aws-modules/terraform-aws-iam"
+repo="https://github.com/terraform-aws-modules/terraform-aws-iam.git"
 v1ref="v3.15.0"
 v2ref="v3.16.0"
 subpath="."
